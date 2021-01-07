736a3f6cc2b59238478e45b599202adc6e46b95d
daabbey1 <dicksonabbey4515@gmail.com>
2021-01-07 13:13:11 +0000

    first commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,174 @@
+@extends('layouts.main')
+@section('title','Prof. Assase | Home')
+@section('body')
+
+
+	<div class="row">
+		<div class="section-title">
+ 			<h2>About</h2>
+		</div>
+	</div>
+	
+ <div class="row home-body-about">
+ 	<!-- about -->
+ 	<div class="col-md-6">
+ 		<img src="{{url('/').'/images/prof cut.png'}}" width="100%" height="80%">
+ 	</div>
+
+ 	<div class="col-md-6 about-text">
+ 		<p class="">Rexford Assasie Oppong (PhD) is currently a Full Professor of Architecture and Dean of International Programmes Office of Kwame Nkrumah University of Science and Technology (KNUST), Kumasi. He is a practicing architect of over twenty years’ experience. He obtained his Doctor of Philosophy in Architecture from the premier Liverpool School of Architecture - University of Liverpool; Masters in Urban Planning and Management from University of Rome-La Sapienza; Postgraduate Diploma in Architecture, KNUST; and Bachelor of Science in Design, UST, Kumasi.
+		He has unwavering passion for architectural education and wrote the initial proposal and subsequently chaired the committee to introduce the Doctor of Philosophy in Architecture in KNUST in 2014. He serves on high profile institutional Councils, Committees and Bodies. He currently represents the National Council for Tertiary Education on the Governing Council of Koforidua Technical University in Ghana. He is member of KNUST Executive Committee, Planning and Resources Committee.
+		He is on the Academic Board of KNUST as one of longest serving members: having served as non-Professorial, Professorial as well as Dean of International Programmmes. Professor Assasie Oppong’s passion for teaching and grooming young generation is expressed through his strict discipline founded on ethical life and behavior. He teaches Architectural History at the Undergraduate level and Philosophy of Social Sciences, Architectural History and Theory as well as Research Methods at the MPhil/ PhD levels in the Department of Architecture, KNUST. His teaching services transcend the boundaries of the University.
+		He is a traveller and sojourner. He believes in Presbyterian discipline and a member of the Christ Congregation Presbyterian Church of Ghana, KNUST. He is married to Mrs. Lydia Assasie Oppong who is the mother of his Trio: Afua Sakyiwaa, Yaw Kyei and Afua Frempomaa.
+		</p>
+ 	</div>
+ 	
+ </div> 
+
+ <div class="row work-section">
+ 	<div class="col-md-6">
+ 		<div class="work-items">
+ 			<p> <strong>2017 to date:</strong> 	Dean – International Programmes Office (IPO), KNUST </p>
+ 		</div>
+ 		<div class="work-items">
+ 			<p> <strong>2013 to date:</strong> 	Associate Professor of Architecture - KNUST  </p>
+ 		</div>
+ 		<div class="work-items">
+ 			<p> <strong>2012 to 2017: </strong> Coordinator – Postgraduate Programmes in Department of Architecture-KNUST  </p>
+ 		</div>
+ 		<div class="work-items">
+ 			<p> <strong>2017 to date:</strong> Chairman – Board of International Programmes Office Kwame Nkrumah University of Science and Technology, Kumasi-Ghana</p>
+ 		</div>
+ 		<div class="work-items">
+ 			<p> <strong>2016 - 2018: </strong>	Chairman – Council of Convocation of Kwame Nkrumah University of Science and Technology, Kumasi-Ghana   </p>
+ 		</div>
+ 		<div class="work-items">
+ 			<p> <strong>2016 to 2018: </strong>Chairman – Procurement Committee of Kwame Nkrumah University of Science and Technology, Kumasi-Ghana  </p>
+ 		</div>
+
+ 		<div class="work-items">
+ 			<p> <strong>2017 to date: </strong>Member – Executive Committee, Kwame Nkrumah University of Science and Technology, Kumasi-Ghana   </p>
+ 		</div>
+ 		<div class="work-items">
+ 			<p> <strong>2017 to date: </strong>Member – Planning and Resources Committee, Kwame Nkrumah University of Science and Technology, Kumasi   </p>
+ 		</div>
+
+ 		
+ 		
+ 	</div>
+ 	<div class="col-md-6">
+ 		<div class="work-items">
+ 			<p> <strong>2016 to 2018: </strong>	Member – Graduate Committee; College of Art and Built Environment of Kwame Nkrumah University of Science and Technology, Kumasi-Ghana  </p>
+ 		</div>	
+ 		<div class="work-items">
+ 			<p> <strong>2016 to date: </strong>	Member – Interdisciplinary Research Working Teams (IRWT) of College of Art and Built Environment, Kwame Nkrumah University of Science and Technology, Kumasi-Ghana   </p>
+ 		</div>	
+ 		<div class="work-items">
+ 			<p> <strong>2019: </strong>	Member - Search Committee for the Appointment of VICE-CHNACELLOR of Koforidua Technical University, Koforidua, E/R. Ghana    </p>
+ 		</div>	
+ 		<div class="work-items">
+ 			<p> <strong>2018 to date: </strong>	Director - Board of Directors Asante Akyem Rural Bank Limited, Juansa-Ashanti </p>
+ 		</div>	
+ 		<div class="work-items">
+ 			<p> <strong>2017 to date: </strong>	Governing Council Member - Koforidua Technical University Council, Eastern Region, Ghana representing National Council for Tertiary Education [NCTE]  </p>
+ 		</div>	
+ 		<div class="work-items">
+ 			<p> <strong>2017</strong> Reviewer – Procurement and Construction of 300-capacity Hostel accommodation project for P.N.M.T.C –Agogo in the Asante Akyem North District, Ashanti Region.  </p>
+ 		</div>	
+ 	
+ 	</div>
+ </div> 
+
+  <div class="row others-section">
+ 	<div class="col-md-6">
+ 	
+ 		<div class="work-items">
+ 			<p> <strong>2016 to 2018: </strong>Chairman – Goods and Services Sub-committee of Entity Tender Committee of Kwame Nkrumah University of Science and Technology, Kumasi-Ghana    </p>
+ 		</div>
+ 		<div class="work-items">
+ 			<p> <strong>2016 to date: </strong>Professorial Member – Academic Board of Kwame Nkrumah University of Science and Technology, Kumasi-Ghana     </p>
+ 		</div>
+ 		<div class="work-items">
+ 			<p> <strong>2016 to date: </strong>Professorial Member – Board of College of Art and Built Environment (CABE) of Kwame Nkrumah University of Science and Technology, Kumasi-Ghana      </p>
+ 		</div>
+ 		<div class="work-items">
+ 			<p> <strong>2016 to date: </strong>	Professorial Member – Board of Faculty of Built Environment (CABE) of Kwame Nkrumah University of Science and Technology, Kumasi-Ghana      </p>
+ 		</div>
+ 		<div class="work-items">
+ 			<p> <strong>2016 to date: </strong>	Member – Welfare Services Board of Kwame Nkrumah University of Science and Technology, Kumasi-Ghana       </p>
+ 		</div>
+ 		<div class="work-items">
+ 			<p> <strong>2016 to date: </strong>	Member – Health Management Services Committee of Kwame Nkrumah University of Science and Technology, Kumasi-Ghana </p>
+ 		</div>
+ 		
+ 		
+ 	</div>
+ 	<div class="col-md-6">
+ 		
+ 		<div class="work-items">
+ 			<p> <strong>2016 to date</strong> Member – Area Board of the Presbyterian Health Service, Ghana  </p>
+ 		</div>	
+ 		<div class="work-items">
+ 			<p> <strong>2007 to date</strong> Consultant/Architect to Christ Congregation (KNUST) of the Presbyterian Church of Ghana.  </p>
+ 		</div>	
+ 		<div class="work-items">
+ 			<p> <strong>2006 to 2012</strong> Consultant/Architect to Asawinso Rural Bank Limited, Asawinso, W/R, Ghana (Pro Bono).
+  			</p>
+ 		</div>	
+ 		<div class="work-items">
+ 			<p> <strong>2006 to 2012</strong> Senior Lecturer of Architecture – KNUST </p>
+ 		</div>
+ 		<div class="work-items">
+ 			<p> <strong>2006 </strong> Resource Person to Ghana Institute of Architects Annual General Meeting held at the KAMA Conference Centre, Accra, Ghana. </p>
+ 		</div>
+ 		<div class="work-items">
+ 			<p> <strong>2000 to 2006 </strong> Lecturer in the Department of Architecture – KNUST. </p>
+ 		</div>
+ 		<div class="work-items">
+ 			<p> <strong>1999 to 2000 </strong> 	Research Fellow - Department of Housing and Planning Research, KNUST . </p>
+ 		</div>
+ 	</div>
+ </div> 
+
+ <div class="row">
+ 	<div class="col-md-4">
+ 		<div class="box">
+ 			<p><strong>Honorary Research Fellow of Liverpool School of Architecture of the University of Liverpool, United Kingdom, 2011 to date.</strong></p>
+ 			<p style="color: grey">Fellowships</p>
+ 		</div>
+ 	</div>
+ 	<div class="col-md-4">
+ 		<div class="box">
+ 			<p><strong>Associate Member of Ghana Institute of Architects (GIA) Standing Register No: ARC S. 0561	</strong></p>
+ 			<p style="color: grey">Affiliations</p>
+ 		</div>
+ 	</div>
+ 	<div class="col-md-4">
+ 		<div class="box">
+ 			<p>Doctor of Philosophy Architecture Programme
+2016/2017 Academic Year
+•	A Theoretical Inquiry into Ecological Aesthetics: The Missing Elements of Urban Green Infrastructure-The Case of Ghana by Ayisha Ida Haruna (Completed).
+•	Architectural Ornamentation and Cultural Sensibilities of Public Buildings in Ghana by Kofi Adjei (final draft submitted).
+</p>
+ 			<p style="color: grey">Mentorships</p>
+ 		</div>
+ 	</div>
+ 	
+ </div>
+
+
+
+
+ <script type="text/javascript">
+		const parallax = document.getElementByClassName("work-items");
+
+		window.addEventListener("scroll", function(){
+			let offset = window.pageYOffset;
+			parallax.style.backgroundPositionY = offset * 0.7 + "px";
+		});
+	</script>
+      
+
+   
+             
+@endsection
